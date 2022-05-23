d0adbe8c2739f03af5f3c7e1ecbed24b67fd51b3
JanKubesIsBest <71927636+JanKubesIsBest@users.noreply.github.com>
2022-05-23 21:05:55 +0200

    Delete myFirstMacGame directory

--- myFirstMacGame/src/body/collision_dot.py
+++
@@ -1,8 +0,0 @@
-from src.body.body.bodyClass import body
-
-class collisionDot(body):
-    def __init__(self, id: int, positionX: int, positionY: int, parentReference: object, group: str):
-        super().__init__(id, positionX, positionY, parentReference)
-
-        self.group = group 
-
